b7352129a27af400ca168a77ff17aa4a1a864f41
Rich Seviora <richard.seviora@gmail.com>
2021-12-22 19:46:20 -0800

    Got it working

--- src/receiver.ts
+++ src/receiver.ts
@@ -1,26 +1,54 @@
-import {config} from 'dotenv';
-import {Client, Intents} from "discord.js";
-
+import { config } from "dotenv";
+import { Client, Intents } from "discord.js";
 
 config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const ephemeralMessages = process.env.DISCORD_HIDDEN != null;
 
-if (typeof process.env.DISCORD_TOKEN !== 'string') {
+if (typeof process.env.DISCORD_TOKEN !== "string") {
   throw new Error("No Discord Token provided");
 }
 
-client.once('ready', () => {
-  console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
 
-  if (commandName === 'rename') {
-    await interaction.reply('Pong!');
+  if (commandName === "rename") {
+    const targetUser = interaction.options.getUser("user");
+    const newName = interaction.options.getString("name");
+    if (targetUser == null || newName == null) {
+      await interaction.reply({
+        content: "missing either name or user",
+        ephemeral: true,
+      });
+      return;
+    }
+    const guild = interaction.guild;
+    if (guild == null) {
+      await interaction.reply({
+        content: "not in guild context or something",
+        ephemeral: true,
+      });
+      return;
+    }
+    const guildMembers = guild.members;
+    try {
+      await guildMembers.edit(targetUser, {
+        nick: newName,
+      });
+      await interaction.reply({
+        content: "changed!",
+        ephemeral: ephemeralMessages,
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }
 });
 
--- src/register-command.ts
+++ src/register-command.ts
@@ -1,44 +1,45 @@
-import {config} from 'dotenv';
+import { config } from "dotenv";
 import * as REST from "@discordjs/rest";
-import {Routes} from "discord-api-types/v9";
-import {SlashCommandBuilder} from "@discordjs/builders";
+import { Routes } from "discord-api-types/v9";
+import { SlashCommandBuilder } from "@discordjs/builders";
 
 config();
-// Place your client and guild ids here
-const clientId = '921269713345069077';
+
+const clientId = "921269713345069077";
 const guildId = process.env.DISCORD_GUILD_ID;
-if (typeof guildId !== 'string') {
-  throw new Error("Guild ID missing")
-}
 const botToken = process.env.DISCORD_TOKEN;
-if (typeof botToken !== 'string') {
-  throw new Error("Bot Token missing")
+if (typeof guildId !== "string") {
+  throw new Error("Guild ID missing");
+}
+
+if (typeof botToken !== "string") {
+  throw new Error("Bot Token missing");
 }
 
 const data = new SlashCommandBuilder()
-  .setName('rename')
-  .setDescription('Renames the user')
-  .addUserOption(option => {
-    return option.setName('user').setDescription('the user to rename').setRequired(true)
+  .setName("rename")
+  .setDescription("Renames the user")
+  .addUserOption((option) => {
+    return option
+      .setName("user")
+      .setDescription("the user to rename")
+      .setRequired(true);
   })
-  .addStringOption(option =>
-    option.setName('name')
-      .setDescription('The new user name')
-      .setRequired(true));
+  .addStringOption((option) =>
+    option.setName("name").setDescription("The new user name").setRequired(true)
+  );
 
-
-const rest = new REST.REST({version: '9'}).setToken(botToken);
+const rest = new REST.REST({ version: "9" }).setToken(botToken);
 
 (async () => {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log("Started refreshing application (/) commands.");
 
-    await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      {body: [data.toJSON()]},
-    );
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: [data.toJSON()],
+    });
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
     console.error(error);
   }
